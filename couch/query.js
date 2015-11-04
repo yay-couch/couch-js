7ed8acf939fad136e7e59aa7603d78bdb8cc1d0d
Kerem Gunes <qeremy@gmail.com>
2015-11-04 21:39:34 +0300

    Add Query object.

---
+++ couch/query.js
@@ -0,0 +1,47 @@
+var Class = require("./util/class");
+
+var Query = Class.create({
+    data: {},
+    dataString: "",
+
+    init: function(data){
+        this.data = data || {};
+        this.dataString = '';
+    },
+
+    set: function(key, value) {
+        this.data[key.toLowerCase()] = value;
+    },
+    get: function(key){
+        return this.data[key];
+    },
+    toString: function(){
+        if (this.dataString != "") {
+            return this.dataString;
+        }
+
+        var data = [], key, value;
+        for (var key in this.data) {
+            value = this.data[key];
+            if (typeof value == "undefined") {
+                continue;
+            }
+            if (value === true || value === false) {
+                data[key] = value ? "true" : "false";
+            }
+            data.push(encodeURIComponent(key) +"="+ encodeURIComponent(value));
+        }
+
+        return (this.dataString = data.join("&"));
+    },
+    skip: function(num){
+        this.data.skip = num;
+        return this;
+    },
+    limit: function(num){
+        this.data.limit = num;
+        return this;
+    }
+});
+
+module.exports = Query;
