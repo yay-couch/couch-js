666a51705d17f60e0b5f643a8b02fa59e0fc1609
Kerem Gunes <qeremy@gmail.com>
2015-11-05 23:35:53 +0300

    Add config methods.

--- couch/couch.js
+++ couch/couch.js
@@ -4,12 +4,22 @@ Couch.VERSION = "1.0";
 
 Couch.Util = require("./util/util");
 
-Couch.Couch = function(config) {
+Couch.Couch = function(config){
     this.config = {};
-    if (config) {
-        this.config = Couch.Util.extend(this.config, config);
-    }
 };
+
+Couch.Util.extend(Couch.Couch.prototype, {
+    setConfig: function(config){
+        if (config) {
+            for (var i in config) {
+                this.config[i] = config[i];
+            }
+        }
+    },
+    getConfig: function(){
+        return this.config;
+    }
+});
 
 Couch.Client = require("./client");
 
