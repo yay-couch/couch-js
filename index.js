43cbdab675f45530745a1f7ae7d27d806481503c
Kerem Gunes <qeremy@gmail.com>
2015-11-05 23:14:07 +0300

    Tests..

--- index.js
+++ index.js
@@ -11,24 +11,31 @@ var Couch = require('./couch/couch');
 var couch = new Couch.Couch();
 var client = new Couch.Client(couch);
 
-// client.request("GET /").done(function(req, res){
+// client.request("POST /_aaa").done(function(req, res){
 //     // log(req)
-//     log(req.headers)
+//     // log(req.headers)
+//     // log(req.body)
+//     // log(req.toString())
+//     // log("---")
 //     // log(res)
-//     log(res.headers)
+//     // log(res.headers)
+//     // log(res.body)
+//     // log(res.toString())
 // });
 
 client.request({
-    method: "PUT",
+    method: "HEAD",
     uri: "/",
     uriParams: {a:1},
     body: {b:2},
     headers: {"X-Foo":"The foo!"}
 }).done(function(req, res){
-    log(req.headers)
-    log(req.body)
+    // log(req.headers)
+    // log(req.body)
+    log(req.toString())
     log("---")
-    log(res.headers)
-    log(res.body)
+    // log(res.headers)
+    // log(res.body)
+    log(res.toString())
 });
 
