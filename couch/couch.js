0dddb62495d25e333a55d0a0f2571336a2f543e9
Kerem Gunes <qeremy@gmail.com>
2015-11-05 06:46:26 +0300

    Trivial.

--- couch/couch.js
+++ couch/couch.js
@@ -2,16 +2,15 @@ var Couch = {};
 Couch.NAME = "CouchJS";
 Couch.VERSION = "1.0";
 
-var Util = require("./util/util");
+Couch.Util = require("./util/util");
 
 Couch.Couch = function(config) {
     this.config = {};
     if (config) {
-        this.config = Util.extend(this.config, config);
+        this.config = Couch.Util.extend(this.config, config);
     }
 };
 
-Couch.Util = Util;
 Couch.Client = require("./client");
 
 module.exports = Couch;
