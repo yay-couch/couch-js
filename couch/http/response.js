3a8989129fcdd6f6300b575d59fc32f7f06625ce
Kerem Gunes <qeremy@gmail.com>
2015-11-05 21:53:01 +0300

    Add CouchDB statuses.

--- couch/http/response.js
+++ couch/http/response.js
@@ -37,7 +37,22 @@ Class.extend(Response, {
 });
 
 Response.STATUS = {
-    OK: 200
+    OK: {200: "OK"},
+    CREATED: {201: "Created"},
+    ACCEPTED: {202: "Accepted"},
+    NOT_MODIFIED: {304: "Not Modified"},
+    BAD_REQUEST: {400: "Bad Request"},
+    UNAUTHORIZED: {401: "Unauthorized"},
+    FORBIDDEN: {403: "Forbidden"},
+    NOT_FOUND: {404: "Not Found"},
+    RESOURCE_NOT_ALLOWED: {405: "Resource Not Allowed"},
+    NOT_ACCEPTABLE: {406: "Not Acceptable"},
+    CONFLICT: {409: "Conflict"},
+    PRECONDITION_FAILED: {412: "Precondition Failed"},
+    BAD_CONTENT_TYPE: {415: "Bad Content Type"},
+    REQUESTED_RANGE_NOT_SATISFIABLE: {416: "Requested Range Not Satisfiable"},
+    EXPECTATION_FAILED: {417: "Expectation Failed"},
+    INTERNAL_SERVER_ERROR: {500: "Internal Server Error"}
 };
 
 module.exports = Response;
