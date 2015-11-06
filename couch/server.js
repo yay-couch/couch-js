32eb9e0a2d8507fe41d2cf9515cb983c2e16a33c
Kerem Gunes <qeremy@gmail.com>
2015-11-06 06:03:37 +0300

    Complete Server object.

--- couch/server.js
+++ couch/server.js
@@ -2,14 +2,12 @@ var Class = require("./util/class");
 
 var Server = Class.create("Server", {
     client: null,
-
     __init__: function(client){
         if (!client) {
             throw new Error("Client must me instance of Couch.Client!");
         }
         this.client = client;
     },
-
     ping: function(callback){
         return this.client.head("/", null, callback);
     },
