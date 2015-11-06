3231ce79f33a3006ac9970a9eab2a5979a5b5aea
Kerem Gunes <qeremy@gmail.com>
2015-11-06 04:47:49 +0300

    Change for "Pass response data into callback".

--- couch/server.js
+++ couch/server.js
@@ -22,33 +22,26 @@ var Server = Class.create("Server", {
         return this.info("version", callback);
     },
     getActiveTasks: function(callback){
-        return this.client.get("/_active_tasks", {}, function(stream){
-            return callback(stream, stream.response.getData());
-        });
+        return this.client.get("/_active_tasks", {}, callback);
     },
     getAllDatabases: function(callback){
-        return this.client.get("/_all_dbs", {}, function(stream){
-            return callback(stream, stream.response.getData());
-        });
+        return this.client.get("/_all_dbs", {}, callback);
     },
     getDatabaseUpdates: function(query, callback){
-        return this.client.get("/_db_updates", {uriParams: query}, function(stream){
-            return callback(stream, stream.response.getData());
-        });
+        return this.client.get("/_db_updates", {uriParams: query}, callback);
     },
     getLogs: function(query, callback){
-        return this.client.get("/_log", {uriParams: query}, function(stream){
-            return callback(stream, stream.response.getData());
-        });
+        return this.client.get("/_log", {uriParams: query}, callback);
     },
     replicate: function(query, callback) {
         query = query || {};
         if (!query.source || !query.target) {
             throw new Error("Both source & target required!");
         }
-        return this.client.post("/_replicate", {body: query}, function(stream){
-            return callback(stream, stream.response.getData());
-        });
+        return this.client.post("/_replicate", {body: query}, callback);
+    },
+    restart: function(callback){
+        return this.client.post("/_restart", null, callback);
     }
 });
 
