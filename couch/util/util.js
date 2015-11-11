c1dc73c3c56022474fbc22d6cc765a11f86bb355
Kerem Gunes <qeremy@gmail.com>
2015-11-12 00:49:39 +0200

    Add file check.

--- couch/util/util.js
+++ couch/util/util.js
@@ -50,21 +50,23 @@ var Util = {
         return output;
     },
     fileInfo: function(file){
-        if (this.fileExists(file)) {
-            var mime, charset, extension, i;
-            if ((i = file.lastIndexOf(".")) > -1) {
-                extension = file.substring(i + 1);
+        if (!this.fileExists(file)) {
+            throw new Error("Could not open file `"+ file +"`!");
+        }
+
+        var mime, charset, extension, i;
+        if ((i = file.lastIndexOf(".")) > -1) {
+            extension = file.substring(i + 1);
+        }
+        var tmp = this.execSync("file -i '"+ file +"' | awk '{print $2} {print $3}'");
+        if (tmp) {
+            tmp = tmp.trim().split("\n");
+            if (tmp.length == 2) {
+                mime = (tmp[0].lastIndexOf(";") > -1)
+                    ? tmp[0].trim().substring(0, tmp[0].length - 1) : tmp[0];
+                charset = tmp[1].trim().split("=")[1];
             }
-            var tmp = this.execSync("file -i '"+ file +"' | awk '{print $2} {print $3}'");
-            if (tmp) {
-                tmp = tmp.trim().split("\n");
-                if (tmp.length == 2) {
-                    mime = (tmp[0].lastIndexOf(";") > -1)
-                        ? tmp[0].trim().substring(0, tmp[0].length - 1) : tmp[0];
-                    charset = tmp[1].trim().split("=")[1];
-                }
-                return {mime: mime, charset: charset, extension: extension};
-            }
+            return {mime: mime, charset: charset, extension: extension};
         }
     },
     fileExists: function(file){
