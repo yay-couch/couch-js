b41374074679b463b881d44ddb39f24600685011
Kerem Gunes <qeremy@gmail.com>
2015-11-11 04:09:11 +0200

    Trivial.

--- index.js
+++ index.js
@@ -222,6 +222,7 @@ var database = new Couch.Database(client, "foo");
 var db = new Couch.Database(client, "foo2"),
     doc = new Couch.Document(db);
 doc._id = "attc_test";
+doc._rev = "1-attc_test";
 // doc.findAttachments(true, ["2-6a0508cce9d2b4f3b83159648415c5e0"], function(s, data){
 //     log(data)
 // })
