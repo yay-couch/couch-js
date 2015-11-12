104b991e684bcf6b05626778555dfe43db1a8fdb
Kerem Gunes <qeremy@gmail.com>
2015-11-12 04:13:20 +0200

    Add docs.

--- couch/http/request.js
+++ couch/http/request.js
@@ -35,7 +35,7 @@ var http = require("http");
 
 /**
  * Request object.
- * @public
+ *
  * @extends Couch.Stream
  */
 var Request = Class.create("Request", {
@@ -58,8 +58,7 @@ var Request = Class.create("Request", {
     uri: undefined,
 
     /**
-     * Object constructor.
-     * @constructor
+     * Object constructor. @constructor
      * @param {Couch.Client} client
      */
     __init__: function(client){
