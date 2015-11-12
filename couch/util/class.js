0a8adde241a98fa008d4b0621b3356f8a008c445
Kerem Gunes <qeremy@gmail.com>
2015-11-12 17:23:34 +0200

    Add docs.

--- couch/util/class.js
+++ couch/util/class.js
@@ -18,34 +18,72 @@
  * limitations under the License.
  */
 
-module.exports = (function() {
+/**
+ * Class object.
+ * @public
+ *
+ * @module  Couch
+ * @object  Couch.Class
+ * @extends Couch.Stream
+ * @author  Kerem Güneş <qeremy[at]gmail[dot]com>
+ */
+var Class = (function() {
     return {
+        /**
+         * Create a fresh class.
+         * @public
+         *
+         * @param  {String} name
+         * @param  {Object} prototype
+         * @return {Object}
+         */
         create: function(name, prototype){
+            // internal
             function Class() {
                 // should be function
                 if (this.__init__) {
                     this.__init__.apply(this, arguments);
                 }
             }
+            // add prototype & constructor
             Class.prototype = prototype;
             Class.prototype.constructor = Class;
-            // just for debug
+
+            // add constructor original name
+            // just for debug cos constructor.name is readonly
             Class.prototype.constructor.nameOrig = name;
+
             return Class;
         },
+
+        /**
+         * Extend an existing class.
+         * @public
+         *
+         * @param  {Object} target
+         * @param  {Object} source
+         * @return {Object}
+         */
         extend: function(target, source) {
             for (var i in source) {
                 // skip private stuff
                 if (0 === i.indexOf("_")) {
                     continue;
                 }
+
                 if (target.prototype) {
                     target.prototype[i] = source[i];
                 } else {
                     target[i] = source[i];
                 }
             }
+
             return target;
         }
     };
 })();
+
+/**
+ * Expose module.
+ */
+module.exports = Class;
